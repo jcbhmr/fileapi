a42a01626acf247c1ba4f84be1f5d9cbe36a467d
Jacob Hummer <jcbhmr@outlook.com>
2023-04-21 22:08:39 +0000

    Get tests to start working

--- src/FileReader.js
+++ src/FileReader.js
@@ -6,30 +6,28 @@
 (function () {
   "use strict";
 
-  var fs = require("fs")
-    , EventEmitter = require("events").EventEmitter
-    ;
-
+  var fs = require("fs"),
+    EventEmitter = require("events").EventEmitter;
   function doop(fn, args, context) {
-    if ('function' === typeof fn) {
+    if ("function" === typeof fn) {
       fn.apply(context, args);
     }
   }
 
   function toDataUrl(data, type) {
     // var data = self.result;
-    var dataUrl = 'data:';
+    var dataUrl = "data:";
 
     if (type) {
-      dataUrl += type + ';';
+      dataUrl += type + ";";
     }
 
     if (/text/i.test(type)) {
-      dataUrl += 'charset=utf-8,';
-      dataUrl += data.toString('utf8');
+      dataUrl += "charset=utf-8,";
+      dataUrl += data.toString("utf8");
     } else {
-      dataUrl += 'base64,';
-      dataUrl += data.toString('base64');
+      dataUrl += "base64,";
+      dataUrl += data.toString("base64");
     }
 
     return dataUrl;
@@ -38,25 +36,25 @@
   function mapDataToFormat(file, data, format, encoding) {
     // var data = self.result;
 
-    switch(format) {
-      case 'buffer':
+    switch (format) {
+      case "buffer":
         return data;
         break;
-      case 'binary':
-        return data.toString('binary');
+      case "binary":
+        return data.toString("binary");
         break;
-      case 'dataUrl':
+      case "dataUrl":
         return toDataUrl(data, file.type);
         break;
-      case 'text':
-        return data.toString(encoding || 'utf8');
+      case "text":
+        return data.toString(encoding || "utf8");
         break;
     }
   }
 
   function FileReader() {
     var self = this,
-      emitter = new EventEmitter,
+      emitter = new EventEmitter(),
       file;
 
     self.addEventListener = function (on, callback) {
@@ -64,30 +62,28 @@
     };
     self.removeEventListener = function (callback) {
       emitter.removeListener(callback);
-    }
+    };
     self.dispatchEvent = function (on) {
       emitter.emit(on);
-    }
+    };
 
     self.EMPTY = 0;
     self.LOADING = 1;
     self.DONE = 2;
 
-    self.error = undefined;         // Read only
-    self.readyState = self.EMPTY;   // Read only
-    self.result = undefined;        // Road only
+    self.error = undefined; // Read only
+    self.readyState = self.EMPTY; // Read only
+    self.result = undefined; // Road only
 
     // non-standard
     self.on = function () {
       emitter.on.apply(emitter, arguments);
-    }
+    };
     self.nodeChunkedEncoding = false;
     self.setNodeChunkedEncoding = function (val) {
       self.nodeChunkedEncoding = val;
     };
     // end non-standard
-
-
 
     // Whatever the file object is, turn it into a Node.JS File.Stream
     function createFileStream() {
@@ -102,23 +98,20 @@
         });
       }
 
-
       // The stream exists, do nothing more
       if (file.stream) {
         return;
       }
 
-
       // Create a read stream from a buffer
       if (file.buffer) {
         process.nextTick(function () {
-          stream.emit('data', file.buffer);
-          stream.emit('end');
+          stream.emit("data", file.buffer);
+          stream.emit("end");
         });
         file.stream = stream;
         return;
       }
-
 
       // Create a read stream from a file
       if (file.path) {
@@ -126,11 +119,11 @@
         if (!chunked) {
           fs.readFile(file.path, function (err, data) {
             if (err) {
-              stream.emit('error', err);
+              stream.emit("error", err);
             }
             if (data) {
-              stream.emit('data', data);
-              stream.emit('end');
+              stream.emit("data", data);
+              stream.emit("end");
             }
           });
 
@@ -144,14 +137,10 @@
       }
     }
 
-
-
     // before any other listeners are added
-    emitter.on('abort', function () {
+    emitter.on("abort", function () {
       self.readyState = self.DONE;
     });
-
-
 
     // Map `error`, `progress`, `load`, and `loadend`
     function mapStreamToEmitter(format, encoding) {
@@ -161,17 +150,17 @@
 
       buffers.dataLength = 0;
 
-      stream.on('error', function (err) {
+      stream.on("error", function (err) {
         if (self.DONE === self.readyState) {
           return;
         }
 
         self.readyState = self.DONE;
         self.error = err;
-        emitter.emit('error', err);
+        emitter.emit("error", err);
       });
 
-      stream.on('data', function (data) {
+      stream.on("data", function (data) {
         if (self.DONE === self.readyState) {
           return;
         }
@@ -179,25 +168,25 @@
         buffers.dataLength += data.length;
         buffers.push(data);
 
-        emitter.emit('progress', {
+        emitter.emit("progress", {
           // fs.stat will probably complete before this
           // but possibly it will not, hence the check
-          lengthComputable: (!isNaN(file.size)) ? true : false,
+          lengthComputable: !isNaN(file.size) ? true : false,
           loaded: buffers.dataLength,
-          total: file.size
+          total: file.size,
         });
 
-        emitter.emit('data', data);
+        emitter.emit("data", data);
       });
 
-      stream.on('end', function () {
+      stream.on("end", function () {
         if (self.DONE === self.readyState) {
           return;
         }
 
         var data;
 
-        if (buffers.length > 1 ) {
+        if (buffers.length > 1) {
           data = Buffer.concat(buffers);
         } else {
           data = buffers[0];
@@ -205,18 +194,17 @@
 
         self.readyState = self.DONE;
         self.result = mapDataToFormat(file, data, format, encoding);
-        emitter.emit('load', {
+        emitter.emit("load", {
           target: {
             // non-standard
             nodeBufferResult: data,
-            result: self.result
-          }
+            result: self.result,
+          },
         });
 
-        emitter.emit('loadend');
+        emitter.emit("loadend");
       });
     }
-
 
     // Abort is overwritten by readAsXyz
     self.abort = function () {
@@ -224,20 +212,18 @@
         return;
       }
       self.readyState = self.DONE;
-      emitter.emit('abort');
+      emitter.emit("abort");
     };
 
-
-
-    // 
+    //
     function mapUserEvents() {
-      emitter.on('start', function () {
+      emitter.on("start", function () {
         doop(self.onloadstart, arguments);
       });
-      emitter.on('progress', function () {
+      emitter.on("progress", function () {
         doop(self.onprogress, arguments);
       });
-      emitter.on('error', function (err) {
+      emitter.on("error", function (err) {
         // TODO translate to FileError
         if (self.onerror) {
           self.onerror(err);
@@ -247,18 +233,16 @@
           }
         }
       });
-      emitter.on('load', function () {
+      emitter.on("load", function () {
         doop(self.onload, arguments);
       });
-      emitter.on('end', function () {
+      emitter.on("end", function () {
         doop(self.onloadend, arguments);
       });
-      emitter.on('abort', function () {
+      emitter.on("abort", function () {
         doop(self.onabort, arguments);
       });
     }
-
-
 
     function readFile(_file, format, encoding) {
       file = _file;
@@ -275,7 +259,7 @@
       // Hence we waste a single tick waiting
       process.nextTick(function () {
         self.readyState = self.LOADING;
-        emitter.emit('loadstart');
+        emitter.emit("loadstart");
         createFileStream();
         mapStreamToEmitter(format, encoding);
         mapUserEvents();
@@ -283,18 +267,18 @@
     }
 
     self.readAsArrayBuffer = function (file) {
-      readFile(file, 'buffer');
+      readFile(file, "buffer");
     };
     self.readAsBinaryString = function (file) {
-      readFile(file, 'binary');
+      readFile(file, "binary");
     };
     self.readAsDataURL = function (file) {
-      readFile(file, 'dataUrl');
+      readFile(file, "dataUrl");
     };
     self.readAsText = function (file, encoding) {
-      readFile(file, 'text', encoding);
+      readFile(file, "text", encoding);
     };
   }
 
   module.exports = FileReader;
-}());
+})();
